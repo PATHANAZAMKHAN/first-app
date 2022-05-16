ed0160bd9d26cb9559e6a6fcb06627254729a95b
PATHANAZAMKHAN <pathanazam001@gmail.com>
2022-05-17 00:25:20 +0530

    final commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import logo from "../Images/logo192.png";
 
-export default function Navbar(){
-    return(
-      <nav>
-        <img src={logo} alt='Error loading' className='logo'></img>
-        <p>ReactFacts</p>
-        <p></p>
-      </nav>
-    )
-  }
-  
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={logo} alt='Error loading' className='logo'></img>
+      <h2>ReactFacts</h2>
+      <h3>React Course - Project 1</h3>
+    </nav>
+  )
+}
+
